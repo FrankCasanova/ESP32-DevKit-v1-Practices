888eaf2d9b7d4a4a2c115b91ab1f4e55e198c8c9
Frank Casanova <frankcasanova.info@gmail.com>
2025-07-25 16:36:25 +0200

    add oled-bmp

---
+++ oled-mbp/src/bin/main.rs
@@ -0,0 +1,72 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use embedded_graphics::image::Image;
+use embedded_graphics::prelude::Point;
+use embedded_graphics::Drawable;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{clock::CpuClock, time::Rate};
+use ssd1306::mode::DisplayConfigAsync;
+use ssd1306::{
+    prelude::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306Async,
+};
+use tinybmp::Bmp;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[esp_hal_embassy::main]
+async fn main(_spawner: Spawner) {
+    // generator version: 0.5.0
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timer0 = TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timer0.timer0);
+
+    let i2c_bus = esp_hal::i2c::master::I2c::new(
+        peripherals.I2C0,
+        esp_hal::i2c::master::Config::default().with_frequency(Rate::from_khz(400)),
+    )
+    .unwrap()
+    .with_scl(peripherals.GPIO18)
+    .with_sda(peripherals.GPIO23)
+    .into_async();
+
+    let interface = I2CDisplayInterface::new(i2c_bus);
+
+    // initialize the display
+    let mut display = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().await.unwrap();
+
+    // Include the BMP file data.
+    let bmp_data = include_bytes!("../zelini.bmp");
+
+    // Parse the BMP file.
+    let bmp = Bmp::from_slice(bmp_data).unwrap();
+
+    let image = Image::new(&bmp, Point::new(32, 16));
+    image.draw(&mut display).unwrap();
+    display.flush().await.unwrap();
+
+    loop {
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
+}
